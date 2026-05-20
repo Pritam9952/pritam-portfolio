6b3baa962e53c4bdeb56b0b98e6ce71fe23af359
pritam9952 <pritamnagar2211@gmail.com>
2026-05-20 13:07:57 +0530

    Fix badge component

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -34,7 +34,8 @@ function Badge({
   ...props
 }: React.ComponentProps<"span"> &
   VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
-  const Comp = asChild ? Slot.Root : "span"
+
+  const Comp = asChild ? Slot : "span"
 
   return (
     <Comp
